588a1f3b6365c24c364e3511aef8a4e0779990dc
simon louvet <simon.louvet.zen@gmail.com>
2017-04-12 15:07:58 +0200

    correction TP JSP 4.2 + 4.3

--- JSP/TD_JSP/WebContent/WEB-INF/itemList.jsp
+++ JSP/TD_JSP/WebContent/WEB-INF/itemList.jsp
@@ -9,18 +9,27 @@
 <title>Insert title here</title>
 </head>
 <body>
+<form method="post">
 <table>
 <%
 List<Item> itemList = (List<Item>)request.getAttribute("itemList");
 for(Item item : itemList){
 %>
 <tr>
-<td><input type="checkbox" <%= (item.getDone())?"checked":""%>></td>
+<td>
+<input type="hidden" name=<%="idInput-".concat(item.getId().toString())%> value=<%=item.getId()%>>
+<input type="checkbox" name=<%="checkboxInput-".concat(item.getId().toString())%> <%= (item.getDone())?"checked":""%>></td>
 <td><%= item.getDescription()%></td>
 <tr>
 <%	
 }
 %>
 </table>
+<button name="updateItems">sauvegarder</button>
+</form>
+<form method="post">
+<label for="newItemDescInput">nouvel item : </label><input id="newItemDescInput" name="newItemDescInput" type="text" />
+<button name="addItem">Ajouter un item</button>
+</form>
 </body>
 </html>
--- JSP/TD_JSP/src/fr/imie/ItemListServlet.java
+++ JSP/TD_JSP/src/fr/imie/ItemListServlet.java
@@ -1,8 +1,10 @@
 package fr.imie;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,31 +18,71 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/itemList")
 public class ItemListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ItemListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		request.setAttribute("itemList", request.getSession().getAttribute("itemList"));
-		request.getRequestDispatcher("WEB-INF/itemList.jsp").forward(request, response);
-		
+	public ItemListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+
+		request.setAttribute("itemList", request.getSession().getAttribute("itemList"));
+		request.getRequestDispatcher("WEB-INF/itemList.jsp").forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		List<Item> itemList=null;
+		if(request.getParameter("addItem")!=null){
+			itemList=  (List<Item>) request.getSession().getAttribute("itemList");
+			Item newItem =  new Item();
+			newItem.setDescription(request.getParameter("newItemDescInput"));
+			newItem.setDone(false);
+			itemList.add(newItem);
+		}else if (request.getParameter("updateItems")!=null) {
+			Enumeration<String> params = request.getParameterNames();
+			while (params.hasMoreElements()) {
+				String param = (String) params.nextElement();
+				Pattern pattern = Pattern.compile("idInput-(\\d*)");
+				Matcher matcher = pattern.matcher(param);
+				boolean b = matcher.matches();
+				// si recherche fructueuse
+				if (b) {
+					Integer id=Integer.parseInt(matcher.group(1));
+					String checkedString = request.getParameter("checkboxInput-".concat(id.toString()));
+					Boolean checked = checkedString!=null;
+					itemList=  (List<Item>) request.getSession().getAttribute("itemList");
+					for (Item item : itemList) {
+						if (item.getId()==id){
+							item.setDone(checked);
+							break;
+						}
+					}
+				}
+			}
+			
+		
+		}
+
+		
+		request.setAttribute("itemList", itemList);
+		request.getRequestDispatcher("WEB-INF/itemList.jsp").forward(request, response);
+
 	}
 
 }
